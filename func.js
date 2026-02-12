66235129b8e834e96262c0376f2f83cac58f7639
eggywaps <cyrildizon0@gmail.com>
2026-02-12 15:04:05 +0800

    Initial commit

---
+++ func.js
@@ -0,0 +1,20 @@
+function handleYes() {
+    document.getElementById("yesModal").style.display = "flex";
+}
+
+function handleNo() {
+    document.getElementById("noModal").style.display = "flex";
+}
+
+function closeModal() {
+    document.getElementById("yesModal").style.display = "none";
+    document.getElementById("noModal").style.display = "none";
+}
+
+function showGifs() {
+    document.getElementById("gifModal").style.display = "flex";
+}
+
+function closeGifModal() {
+    document.getElementById("gifModal").style.display = "none";
+}
